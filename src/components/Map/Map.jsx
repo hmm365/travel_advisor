fbbe00bb241df7612f44f7f569828b52808360e9
hmm365 <happyham52@gmail.com>
2022-12-14 10:07:00 +0900

    1214

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './styles';
+const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px');
+    const coordinates = { lat: 0, lng: 0 };
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCxGAwimiPN7Y64-kXlxAbScOR0a3YKNtY' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+            ></GoogleMapReact>
+        </div>
+    );
+};
+// options={''}
+// onChange={''}
+// onChildClick={''}
+export default Map;
