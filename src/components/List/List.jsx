b325a5e5abe368d165ef13f56b8c0170377632d4
hmm365 <happyham52@gmail.com>
2022-12-14 10:06:52 +0900

    1214

---
+++ src/components/List/List.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, Formcontrol, Select, FormControl } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const List = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+        <Typography variant="h4">
+         Restaurants, Hotels & Attractions around you
+        </Typography>
+        <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={} onChange={}>
+                <MenuItem value="restaurants">
+                    Restaurants
+                </MenuItem>
+                <MenuItem value="hotels">
+                    Hotels
+                </MenuItem>
+                <MenuItem value="attractions">
+                    Restaurants
+                </MenuItem>
+            </Select>
+        </FormControl>
+        </div>
+    );
+};
+
+export default List;
