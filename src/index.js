793d88a0729e9ccd604873e57d0c39d937988520
hmm365 <happyham52@gmail.com>
2022-12-15 22:52:01 +0900

    fix: react 17로 다운그레이드

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 
 //17버전
+import ReactDOM from 'react-dom';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 //18버전
+// import ReactDOM from 'react-dom/client'
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //     // <React.StrictMode>
